27249999430ddf6565dc8e5a3a313354343dae5c
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-09-16 14:52:45 +0500

    Initial Commit

---
+++ Foods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foods
+//
+//  Created by Saidaxmad on 16/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
