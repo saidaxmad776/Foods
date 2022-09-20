50412c3ad743109162ae31651fce90898fcf4289
ASaidaxmad <saidaxmad7760746@gmail.com>
2022-09-20 09:56:41 +0500

    Context Menu Create

--- Foods/ContextMenuVC.swift
+++ Foods/ContextMenuVC.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ContextMenuVC: UIViewController {
     
-    private let saleImageView: UIImageView = {
+    private let burgerImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.layer.cornerRadius = 15
         imageView.image = UIImage(named: "image")
@@ -24,13 +24,59 @@ class ContextMenuVC: UIViewController {
         
         setupView()
         setConstraints()
+        requestContextMenu()
     }
     
     private func setupView() {
         
+        view.backgroundColor = .systemGray5
+        view.addSubview(burgerImageView)
+        
+    }
+    
+    private func requestContextMenu() {
+        
+        let contextMenu = UIContextMenuInteraction(delegate: self)
+        burgerImageView.isUserInteractionEnabled = true
+        burgerImageView.addInteraction(contextMenu)
     }
     
     private func setConstraints() {
         
+        NSLayoutConstraint.activate([
+        
+            burgerImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            burgerImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            burgerImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            burgerImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -400),
+        ])
     }
+}
+
+extension ContextMenuVC: UIContextMenuInteractionDelegate {
+    
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
+            
+            let like = UIAction(title: "Like", image: UIImage(systemName: "person")) { _ in
+                print("like")
+            }
+            
+            let subscrime = UIAction(title: "subscrime", image: UIImage(systemName: "person")) { _ in
+                print("subscrime")
+            }
+            
+            let contact = UIAction(title: "contact", image: UIImage(systemName: "person")) { _ in
+                print("contact")
+            }
+            
+            let delete = UIAction(title: "delete", image: UIImage(systemName: "person")) { _ in
+                print("delete")
+            }
+            
+            return UIMenu(title: "Contect Menu", children: [like, subscrime, contact, delete])
+        }
+    }
+    
 }
